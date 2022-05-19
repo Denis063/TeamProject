6721437176beb04defc75ef6a2939c6fe113812b
Денис Сергеев <D-D1983@yandex.ru>
2022-05-19 14:51:06 +0400

    public Game mostPlayerByGenre modified

--- src/main/java/ru/netology/Player.java
+++ src/main/java/ru/netology/Player.java
@@ -58,6 +58,17 @@ public class Player {
     /** Метод принимает жанр и возвращает игру этого жанра, в которую играли больше всего
      Если в игры этого жанра не играли, возвращается null */
     public Game mostPlayerByGenre(String genre) {
-        return null;
+        Game bestPlayer = null;
+        int bestHours = -1;
+        for (Game game : playedTime.keySet()) {
+            if (game.getGenre().equals(genre)) {
+                int currGameHours = playedTime.get(game);
+                if (currGameHours > bestHours) {
+                    bestHours = currGameHours;
+                    bestPlayer = game;
+                }
+            }
+        }
+        return bestPlayer;
     }
 }

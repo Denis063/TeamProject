97a9fdcbef798d6431901e1a5f85f7b50daca645
Денис Сергеев <D-D1983@yandex.ru>
2022-05-19 12:28:01 +0400

    bug1 fixed

--- src/main/java/ru/netology/Player.java
+++ src/main/java/ru/netology/Player.java
@@ -33,9 +33,9 @@ public class Player {
     public int play(Game game, int hours) {
         game.getStore().addPlayTime(name, hours);
         if (playedTime.containsKey(game)) {
-            playedTime.put(game, playedTime.get(game));
+            playedTime.put(game, playedTime.get(game) + hours);
         } else {
-            playedTime.put(game, hours);
+            throw new RuntimeException("Game " + game.getTitle() + " of genre " + game.getGenre() + " is not installed");
         }
         return playedTime.get(game);
     }

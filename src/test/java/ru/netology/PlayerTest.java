c6dca9afa87f999fd89cad809990f743b811b6a7
Денис Сергеев <D-D1983@yandex.ru>
2022-05-19 13:15:56 +0400

    new tests added

--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -5,6 +5,14 @@ import org.junit.jupiter.api.Test;
 
 public class PlayerTest {
 
+    private GameStore store = new GameStore();
+
+    private Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+    private Game game1 = store.publishGame("Разрушитель", "Экшн");
+    private Game game2 = store.publishGame("Расцвет Империи", "Стратегия");
+    private Game game3 = store.publishGame("Война Миров", "Экшн");
+    private Game game4 = store.publishGame("Солдаты удачи", "Экшн");
+
     @Test
     public void shouldSumGenreIfOneGame() {
         GameStore store = new GameStore();
@@ -19,5 +27,91 @@ public class PlayerTest {
         assertEquals(expected, actual);
     }
 
-    // другие ваши тесты
+    @Test
+    public void shouldSumGenreIfTwoGames() {
+        Player player = new Player("Petya");
+        player.installGame(game1);
+        player.play(game1, 7);
+        player.installGame(game3);
+        player.play(game3, 9);
+
+        int expected = 16;
+        int actual = player.sumGenre(game1.getGenre());
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSumGenreIfThreeGames() {
+        Player player = new Player("Petya");
+        player.installGame(game1);
+        player.play(game1, 4);
+        player.installGame(game3);
+        player.play(game3, 7);
+        player.installGame(game4);
+        player.play(game4, 3);
+
+        int expected = 14;
+        int actual = player.sumGenre(game1.getGenre());
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSumGenreIfNoGames() {
+        Player player = new Player("Petya");
+
+        int expected = 0;
+        int actual = player.sumGenre("Логические");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldPlayAtOnce() {
+        Player player = new Player("Игорь");
+        player.installGame(game);
+
+        int expected = 8;
+        int actual = player.play(game, 8);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldPlayMoreThanOnce() {
+        Player player = new Player("Олег");
+        player.installGame(game2);
+        player.play(game2, 1);
+
+        int expected = 6;
+        int actual = player.play(game2, 5);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldPlayIfGameNotInstalled() {
+        Player player = new Player("Дмитрий");
+
+        assertThrows(RuntimeException.class, () -> {
+            player.play(game2, 5);
+        });
+    }
+    @Test
+    public void shouldInstall() {
+        Game game = store.publishGame("Звездный десант", "RTS");
+        Player player = new Player("Евгений");
+        player.installGame(game);
+
+        int expected = 0;
+        int actual = player.sumGenre("RTS");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldChangePlayTimeIfReinstall() {
+        Player player = new Player("Валентин");
+        player.installGame(game);
+        player.play(game, 9);
+        player.installGame(game);   // Переустановка игры
+
+        int expected = 9;
+        int actual = player.sumGenre("Аркады");
+        assertEquals(expected, actual);
+    }
 }

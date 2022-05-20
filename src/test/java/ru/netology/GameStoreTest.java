53575d42a0a3a9cfcca6c1936dd00ac74d1fd5b9
NatashaBazunova <natashaiosifova@mail.ru>
2022-05-20 19:41:28 +0500

    Added Tests

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -1,18 +1,58 @@
 package ru.netology;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class GameStoreTest {
+    GameStore store = new GameStore();
+
 
     @Test
     public void shouldAddGame() {
 
-        GameStore store = new GameStore();
         Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game2 = store.publishGame("Меч и Магия", "Аркады");
+        Game game3 = store.publishGame("Пабг Мобайл", "Шутеры");
+        Game game4 = store.publishGame("2048", "Логические");
 
-        assertTrue(store.containsGame(game));
+
+        assertTrue(store.containsGame(game3));
     }
 
     // другие ваши тесты
+    @Test
+    public void shouldShowPlayTime() {
+        store.addPlayTime("Olya", 12);
+        store.addPlayTime("Sasha", 4);
+        store.addPlayTime("Alex", 10);
+        store.addPlayTime("Nina", 7);
+
+        assertEquals("Olya", store.getMostPlayer());
+    }
+    @Test
+    public void shouldShowPlayTimeOfOnePlayer() {
+        store.addPlayTime("Nina", 7);
+        assertEquals("Nina", store.getMostPlayer());
+    }
+    @Test
+    public void shouldShowSumPlayedTime() {
+        store.addPlayTime("Alex", 10);
+        store.addPlayTime("Evgen", 11);
+        store.addPlayTime("Sasha", 4);
+
+        assertEquals(25, store.getSumPlayedTime());
+    }
+
+    @Test
+    public void shouldShowSumPlayedTimeOfOnePlayer() {
+        store.addPlayTime("Alex", 10);
+        assertEquals(10, store.getSumPlayedTime());
+    }
+
+
+
+
+
 }
